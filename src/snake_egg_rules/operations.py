40fc647821083e93d36c9719aca296ab740db5da
Ian Briggs <ibriggsutah@gmail.com>
2022-01-04 16:26:06 -0800

    Noting domain restrictions

--- src/snake_egg_rules/operations.py
+++ src/snake_egg_rules/operations.py
@@ -3,64 +3,64 @@
 from collections import namedtuple
 
 
+nt = namedtuple
 
+PI        = nt("PI",         "")
+E         = nt("E",          "")
 
-PI = namedtuple("PI", "")
-E = namedtuple("E", "")
+neg       = nt("neg",        "x")
+inv       = nt("inv",        "x")   # x != 0
 
-neg = namedtuple("neg", "x")
+add       = nt("add",        "x y")
+sub       = nt("sub",        "x y")
+mul       = nt("mul",        "x y")
+div       = nt("div",        "x y") # y != 0
 
-add = namedtuple("add", "x y")
-sub = namedtuple("sub", "x y")
-mul = namedtuple("mul", "x y")
-div = namedtuple("div", "x y")
+acos      = nt("acos",       "x")   # -1 <= x <= 1
+acosh     = nt("acosh",      "x")   #  1 <= x
+asin      = nt("asin",       "x")   # -1 <= x <= 1
+asinh     = nt("asinh",      "x")
+atan      = nt("atan",       "x")
+atanh     = nt("atanh",      "x")
+cbrt      = nt("cbrt",       "x")
+cos       = nt("cos",        "x")
+cosh      = nt("cosh",       "x")
+erf       = nt("erf",        "x")
+erfc      = nt("erfc",       "x")
+exp       = nt("exp",        "x")
+expm1     = nt("expm1",      "x")
+fabs      = nt("fabs",       "x")
+log       = nt("log",        "x")   #  0 < x
+log1p     = nt("log1p",      "x")   # -1 < x
+sin       = nt("sin",        "x")
+sinh      = nt("sinh",       "x")
+sqrt      = nt("sqrt",       "x")   #  0 <= x
+tan       = nt("tan",        "x")
+tanh      = nt("tanh",       "x")
 
+atan2     = nt("atan2",      "x y") # x != 0 && y != 0
+pow       = nt("pow",        "x y") # (x < 0 && int(y) == y) || (x == 0 && 0 <= y) || (0 < x)
+hypot     = nt("hypot",      "x y") #  0 <= x && 0 <= y
+remainder = nt("remainder",  "x y") # y != 0
 
-acos = namedtuple("acos", "x")
-acosh = namedtuple("acosh", "x")
-asin = namedtuple("asin", "x")
-asinh = namedtuple("asinh", "x")
-atan = namedtuple("atan", "x")
-atanh = namedtuple("atanh", "x")
-cbrt = namedtuple("cbrt", "x")
-cos = namedtuple("cos", "x")
-cosh = namedtuple("cosh", "x")
-erf = namedtuple("erf", "x")
-erfc = namedtuple("erfc", "x")
-exp = namedtuple("exp", "x")
-expm1 = namedtuple("expm1", "x")
-fabs = namedtuple("fabs", "x")
-log = namedtuple("log", "x")
-log1p = namedtuple("log1p", "x")
-sin = namedtuple("sin", "x")
-sinh = namedtuple("sinh", "x")
-sqrt = namedtuple("sqrt", "x")
-tan = namedtuple("tan", "x")
-tanh = namedtuple("tanh", "x")
-
-atan2 = namedtuple("atan2", "x y")
-pow = namedtuple("pow", "x y")
-hypot = namedtuple("hypot", "x y")
-remainder = namedtuple("remainder", "x y")
-
-fma = namedtuple("fma", "x y z")
+fma       = nt("fma",        "x y z")
 
 # Not in rewrite rules
-uadd = namedtuple("uadd", "x")
-ceil = namedtuple("ceil", "x")
-exp2 = namedtuple("exp2", "x")
-fdim = namedtuple("fdim", "x y")
-floor = namedtuple("floor", "x")
-isnormal = namedtuple("isnormal", "x")
-lgamma = namedtuple("lgamma", "x")
-log10 = namedtuple("log10", "x")
-log2 = namedtuple("log2", "x")
-nearbyint = namedtuple("nearbyint", "x")
-round = namedtuple("round", "x")
-signbit = namedtuple("signbit", "x")
-tgamma = namedtuple("tgamma", "x")
+uadd      = nt("uadd",       "x")
+ceil      = nt("ceil",       "x")
+exp2      = nt("exp2",       "x")
+fdim      = nt("fdim",       "x y")
+floor     = nt("floor",      "x")
+isnormal  = nt("isnormal",   "x")
+lgamma    = nt("lgamma",     "x")   # complicated
+log10     = nt("log10",      "x")   #  0 < x
+log2      = nt("log2",       "x")   #  0 < x
+nearbyint = nt("nearbyint",  "x")
+round     = nt("round",      "x")
+signbit   = nt("signbit",    "x")
+tgamma    = nt("tgamma",     "x")   # complicated
 
-fmax = namedtuple("fmax", "x y")
-fmin = namedtuple("fmin", "x y")
-fmod = namedtuple("fmod", "x y")
+fmax      = nt("fmax",       "x y")
+fmin      = nt("fmin",       "x y")
+fmod      = nt("fmod",       "x y") # y != 0
 
