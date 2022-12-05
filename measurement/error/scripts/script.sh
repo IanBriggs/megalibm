d839c417515e3db06043f5e177628380531fe6ba
Ian Briggs <ianbriggsutah@gmail.com>
2022-12-05 12:14:04 -0700

    Move website generation

--- measurement/error/scripts/script.sh
+++
@@ -1,7 +0,0 @@
-./bin/main_versin 0 > data/versin_error_0.json
-./bin/main_versin 1 > data/versin_error_1.json
-./bin/main_versin 2 > data/versin_error_2.json
-./bin/main_versin 3 > data/versin_error_3.json
-./bin/main_versin 4 > data/versin_error_4.json
-./bin/main_versin 5 > data/versin_error_5.json
-./bin/main_versin 6 > data/versin_error_6.json
