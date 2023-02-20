33bccf6348c39967fb96ef1cc6b296fd59c0bdde
Chandrakana Nandi <chandra.nandi90@gmail.com>
2023-02-20 14:45:24 -0800

    update script

--- src/ruler_processing.py
+++ src/ruler_processing.py
@@ -23,19 +23,20 @@ for l in raw_rules:
 """
 
 usage = """USAGE: python ruler_processing.py <input> <output>
-    <input>  ruler rules json file
+    <input>  ruler rules txt file
     <output> megalibm rules python file
   """
 
 operator_map = {
-  "+ " : "add ",
-  "~ " : "neg ",
-  "- " : "sub ",
-  "* " : "mul ",
-  "/ " : "div ",
-  "tan " : "tan ",
-  "cos " : "cos ",
-  "sin " : "sin ",
+  "+ "    : "add ",
+  "~ "    : "neg ",
+  "- "    : "sub ",
+  "* "    : "mul ",
+  "/ "    : "div ",
+  "fabs " : "fabs ",
+  "tan "  : "tan ",
+  "cos "  : "cos ",
+  "sin "  : "sin ",
 }
 
 def is_operator(s):
@@ -49,7 +50,7 @@ def lisp_to_c_style(sexpr):
     if sexpr == []:
       return ""
     elif len(sexpr) == 1:
-      return dumps(car(sexpr[0]), str_as='symbol') 
+      return dumps(car(sexpr[0]), str_as='symbol')
     else:
       hd = car(sexpr)
       args = cdr(sexpr)
@@ -58,55 +59,48 @@ def lisp_to_c_style(sexpr):
       return hd_s + "(" + ", ".join(args_s) + ")"
 
 def replace_all(sexpr, operator_map):
-  for old, new in operator_map.items() :
-    sexpr = sexpr.replace(old, new)
-  return sexpr
+    for old, new in operator_map.items() :
+        sexpr = sexpr.replace(old, new)
+    return sexpr
 
 def cleanup(expr):
-  expr = expr.replace("?", "")
-  expr = replace_all(expr, operator_map)
-  return str(lisp_to_c_style(loads(expr))).replace("PI", "CONST_PI()")
+    expr = expr.replace("?", "")
+    expr = replace_all(expr, operator_map)
+    return str(lisp_to_c_style(loads(expr))).replace("PI", "CONST_PI()")
 
 @dataclass(repr=False)
 class RulerRule:
-    name: str 
+    name: str
     lhs: str
     rhs: str
-  
+
     def __repr__(self):
-      return f'["{self.name}", {cleanup(self.lhs)}, {cleanup(self.rhs)}]'
+        return f'["{self.name}", {cleanup(self.lhs)}, {cleanup(self.rhs)}]'
 
 def mk_rules(rules, f):
-  # first rule
-  f.write("\n\t" + repr(rules[0]) + ",\n")
-  for i in range(1, len(rules) - 1):
-    f.write("\t" + repr(rules[i]) + ",\n")
-  # last rule
-  f.write("\t" + repr(rules[len(rules) - 1]))  
+    # first rule
+    f.write("\n\t" + repr(rules[0]) + ",\n")
+    for i in range(1, len(rules) - 1):
+        f.write("\t" + repr(rules[i]) + ",\n")
+    # last rule
+    f.write("\t" + repr(rules[len(rules) - 1]))
 
 def process_rules(input, output):
-  with open(output, 'w+') as f:
-    f.write(prelude)
-    count = 0
-    rules = []
-    content = json.loads(open(input, 'r').read())['all_eqs']
-    for c in content:
-      if ("cis " in str(c['lhs'])) or ("cis " in str(c['rhs'])):
-        continue
-      else:
-        if c['bidirectional']:
-          r1 = RulerRule(str(count), c['lhs'], c['rhs'])
-          count += 1
-          rules.append(r1)
-          r2 = RulerRule(str(count), c['rhs'], c['lhs'])
-          count += 1
-          rules.append(r2)
-        else:
-          r = RulerRule(str(count), c['lhs'], c['rhs'])
-          count += 1
-          rules.append(r)
-    mk_rules(rules, f)
-    f.write(epilogue)
+    with open(output, 'w+') as f:
+        f.write(prelude)
+        count = 0
+        rules = []
+        content = open(input, 'r').readlines()
+        for c in content:
+            (lhs, rhs) = c.split("==>")
+            if ("cis " in lhs) or ("cis " in rhs):
+                continue
+            else:
+                r = RulerRule(str(count), lhs, rhs)
+                count += 1
+                rules.append(r)
+        mk_rules(rules, f)
+        f.write(epilogue)
 
 if __name__ == "__main__":
     if (len(sys.argv) < 3):
