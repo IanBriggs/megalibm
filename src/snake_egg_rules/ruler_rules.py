1552ea049e8a74a4f6c9a2311f5e96541894d18d
Amy Zhu <ajyzhu@gmail.com>
2023-02-28 11:22:15 -0800

    Remove per-branch collection

--- src/snake_egg_rules/ruler_rules.py
+++ src/snake_egg_rules/ruler_rules.py
@@ -126,60 +126,62 @@ def scrape_and_grab_json():
     # print(runs_split)
     runs_split.sort(key=lambda run1: int(run1[0]))
 
-    branches = ['nightlies-tests']
-    # branches = list(set([run[2] for run in runs_split]))
+    # branches = ['nightlies-tests']
+    branches = list(set([run[2] for run in runs_split]))
+    # branches = runs_split[-1][2]
     # print(branches)
 
     # timestamp, "nightly", branch, commit
     runs_split_by_branches = reduce(assign_to_branch, runs_split, dict.fromkeys(branches, []))
     # print(runs_split_by_branches)
 
-    for branch_name in branches:
-        all_rules = []    
+    all_rules = [] 
+    for branch_name in branches:   
         relevant_runs = runs_split_by_branches[branch_name]
         latest = relevant_runs[-1]    
         print(f"For branch {branch_name}, found {len(relevant_runs)} runs. Latest at time {latest[0]} with commit {latest[3]}.")    
-        current_attempt = 1
-        while True:
-            try:
-                # ensures that all domains are taken from the same commit
-                for domain in DOMS_TO_COMBINE:
-                    page_to_scrape = url + \
-                        sep.join(latest) + json_folder + domain + ".json"
-                    print(f"Scraping json from {domain}, using {page_to_scrape}")    
-                    json_file = json.load(urlopen(page_to_scrape))
-                    # print(json_file)
-                    all_rules.extend(json_file["rules"])
-                break
-            except HTTPError as err:
-                if err.code == 404:
-                    if current_attempt >= len(relevant_runs):
-                        raise Exception(
-                            f"No json appear to be available for any commits on this branch ({branch_name}).")
-                    current_attempt += 1
-                    print(current_attempt)
-                    latest = relevant_runs[-current_attempt]
-                    print(
-                        f"This commit failed. Trying again with commit {latest[3]} at time {latest[0]}")
-                else:
-                    raise err    
-        
-        # filename = f'{rule_save_path}{branch_name}-{latest[0]}.txt'
-        # with open(filename, 'w') as f:
-        #     f.write("\n".join(all_rules))
-        #     print(f"Saved collated rules into {filename}.")
-        print("Collated rules are:")
-        print("\n".join(all_rules))
-
-        rule_str = process_rules(all_rules)
-        evaled_rules = eval(rule_str)
-        # print(rules)
-        for l in evaled_rules:
-            name = l[0]
-            frm = l[1]
-            to = l[2]
-            global rules
-            rules.append(Rewrite(frm, to, name))
+    latest = runs_split[-1]
+    print(f"Using latest from branch {latest[2]}.")
+    current_attempt = 1
+    while True:
+        try:
+            # ensures that all domains are taken from the same commit
+            for domain in DOMS_TO_COMBINE:
+                page_to_scrape = url + \
+                    sep.join(latest) + json_folder + domain + ".json"
+                print(f"Scraping json from {domain}, using {page_to_scrape}")    
+                json_file = json.load(urlopen(page_to_scrape))
+                # print(json_file)
+                all_rules.extend(json_file["rules"])
+            break
+        except HTTPError as err:
+            if err.code == 404:
+                if current_attempt >= len(relevant_runs):
+                    raise Exception(
+                        f"No json appear to be available for any commits on this branch ({branch_name}).")
+                current_attempt += 1
+                print(current_attempt)
+                latest = relevant_runs[-current_attempt]
+                print(
+                    f"This commit failed. Trying again with commit {latest[3]} at time {latest[0]}")
+            else:
+                raise err    
+    
+    # filename = f'{rule_save_path}{branch_name}-{latest[0]}.txt'
+    # with open(filename, 'w') as f:
+    #     f.write("\n".join(all_rules))
+    #     print(f"Saved collated rules into {filename}.")
+    print("Collated rules are:")
+    print("\n".join(all_rules))
+    rule_str = process_rules(all_rules)
+    evaled_rules = eval(rule_str)
+    # print(rules)
+    for l in evaled_rules:
+        name = l[0]
+        frm = l[1]
+        to = l[2]
+        global rules
+        rules.append(Rewrite(frm, to, name))
 
 
 
