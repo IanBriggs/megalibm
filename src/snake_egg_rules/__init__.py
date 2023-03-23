94835034049638312b9bde31dddf7f78ce5eecd4
Amy Zhu <ajyzhu@gmail.com>
2023-03-22 17:19:50 -0700

    remove sound div

--- src/snake_egg_rules/__init__.py
+++ src/snake_egg_rules/__init__.py
@@ -2,8 +2,8 @@
 # Order is important here
 from .operations import *
 
-from .rules import rules
-# from .ruler_rules_custom_trig import rules
+# from .rules import rules
+from .ruler_rules_custom_trig import rules
 # from .ruler_rules import rules
 
 from .eval import eval
