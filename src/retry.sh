1cb4095f846a0879ddd3657b070461c035eddee1
Ian Briggs <ianbriggsutah@gmail.com>
2023-04-03 12:11:53 -0600

    Fix Retry

--- src/retry.sh
+++ src/retry.sh
@@ -9,6 +9,8 @@ SCRIPT_LOCATION=$(readlink -f "${SCRIPT_DIR}")
 GIT_LOCATION=$(cd "${SCRIPT_LOCATION}" && cd .. && pwd)
 NIGHTLIES_LOCATION=${GIT_LOCATION}/nightlies
 
+CORES="$(getconf _NPROCESSORS_ONLN)"
+
 # check args
 if [ $# != 1 ] ; then
 echo "retry must be given a nightly directory"
@@ -21,24 +23,32 @@ THIS_NIGHTLY_LOCATION="${PWD}/${1}"
 rm -rf "${GIT_LOCATION}/measurement/timing/generated"
 rm -rf "${GIT_LOCATION}/measurement/error/generated"
 
+# Clean data to force a regen
+find "${THIS_NIGHTLY_LOCATION}/generated/" -name "*.json" -delete
+find "${THIS_NIGHTLY_LOCATION}/generated/" -name "*.o" -delete
+find "${THIS_NIGHTLY_LOCATION}/generated/" -name "*.png" -delete
+find "${THIS_NIGHTLY_LOCATION}/generated/" -name "error_main" -delete
+find "${THIS_NIGHTLY_LOCATION}/generated/" -name "timing_main" -delete
+
 # Move generated to timing dir
-mv "${THIS_NIGHTLY_LOCATION}/generated" "${GIT_LOCATION}/measurement/timing/"
+cp -r "${THIS_NIGHTLY_LOCATION}/generated" "${GIT_LOCATION}/measurement/timing"
 
 # Time functions
-cd "${GIT_LOCATION}/measurement/timing/"
+cd "${GIT_LOCATION}/measurement/timing"
 make -j"${CORES}" build
 make -j1 run
 
 # Move generated to error dir
-mv "${GIT_LOCATION}/measurement/timing/generated" "${GIT_LOCATION}/measurement/error/"
+mv "${GIT_LOCATION}/measurement/timing/generated" "${GIT_LOCATION}/measurement/error"
 
 # Error measurement
-cd "${GIT_LOCATION}/measurement/error/"
+cd "${GIT_LOCATION}/measurement/error"
 make -j"${CORES}" build
 make -j"${CORES}" run
 
 # Move generated to final directory
-mv "${GIT_LOCATION}/measurement/error/generated" "${THIS_NIGHTLY_LOCATION}/generated/"
+rm -r "${THIS_NIGHTLY_LOCATION}/generated"
+mv "${GIT_LOCATION}/measurement/error/generated" "${THIS_NIGHTLY_LOCATION}/generated"
 
 # Generate website
 cd "${THIS_NIGHTLY_LOCATION}"
