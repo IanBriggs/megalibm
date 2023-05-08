f93b0692cbcf9853bc6186d29cf50735176d4129
Ian Briggs <ianbriggsutah@gmail.com>
2023-05-08 15:47:19 -0600

    Improve include-libs

--- double-double/include/double_double.h
+++ double-double/include/double_double.h
@@ -1,3 +1,29 @@
+/******************************************************************************/
+/*  ____   __   _  _  ____  __    ____                                        */
+/* (    \ /  \ / )( \(  _ \(  )  (  __)                                       */
+/*  ) D ((  O )) \/ ( ) _ (/ (_/\ ) _)                                        */
+/* (____/ \__/ \____/(____/\____/(____)                                       */
+/*          ____   __   _  _  ____  __    ____     _  _                       */
+/*         (    \ /  \ / )( \(  _ \(  )  (  __)   / )( \                      */
+/*    ____  ) D ((  O )) \/ ( ) _ (/ (_/\ ) _)  _ ) __ (                      */
+/*   (____)(____/ \__/ \____/(____/\____/(____)(_)\_)(_/                      */
+/*                                                                            */
+/* Compute in extra precision!                                                */
+/*                                                                            */
+/******************************************************************************/
+/*  _    _  ___  ______ _   _ _____ _   _ _____  _____                        */
+/* | |  | |/ _ \ | ___ \ \ | |_   _| \ | |  __ \/  ___|_                      */
+/* | |  | / /_\ \| |_/ /  \| | | | |  \| | |  \/\ `--.(_)                     */
+/* | |/\| |  _  ||    /| . ` | | | | . ` | | __  `--. \                       */
+/* \  /\  / | | || |\ \| |\  |_| |_| |\  | |_\ \/\__/ /_                      */
+/*  \/  \/\_| |_/\_| \_\_| \_/\___/\_| \_/\____/\____/(_)                     */
+/* (inherited from double_access.h)                                           */
+/* 1. Assumes IEEE754 binary double                                           */
+/* 2. Does not care about endianess                                           */
+/* 3. May have undefined behavior depending on language standard and compile  */
+/*    time setting used                                                       */
+/******************************************************************************/
+
 #ifndef DOUBLE_DOUBLE_H
 #define DOUBLE_DOUBLE_H
 
@@ -5,117 +31,153 @@
 #include "double_access.h"
 #include "double_helpers.h"
 
-#include <math.h> // for fma()
+#include <math.h> /* for fma() */
 
-// Based on:
-// "CR-LIBM A library of correctly rounded elementary functions in double-precision"
-// https://en.wikipedia.org/wiki/2Sum
-// "Library for Double-Double and Quad-Double Arithmetic"
+/* Based on:                                                                  */
+/* "CR-LIBM A library of correctly rounded elementary functions in            */
+/*   double-precision"                                                        */
+/* https://en.wikipedia.org/wiki/2Sum                                         */
+/* "Library for Double-Double and Quad-Double Arithmetic"                     */
 
-// WARNINGS: (inherited from double-access.h)
-// * NO HEED IS GIVEN TO ENDIANESS!!!
-// * UB may be present
+/******************************************************************************/
+/*   ___  __   _  _  ____  __  __    ____    ____  __  _  _  ____             */
+/*  / __)/  \ ( \/ )(  _ \(  )(  )  (  __)  (_  _)(  )( \/ )(  __)            */
+/* ( (__(  O )/ \/ \ ) __/ )( / (_/\ ) _)     )(   )( / \/ \ ) _)             */
+/*  \___)\__/ \_)(_/(__)  (__)\____/(____)   (__) (__)\_)(_/(____)            */
+/*  ____  ____  ____  ____  __  __ _   ___  ____                              */
+/* / ___)(  __)(_  _)(_  _)(  )(  ( \ / __)/ ___)                             */
+/* \___ \ ) _)   )(    )(   )( /    /( (_ \\___ \                             */
+/* (____/(____) (__)  (__) (__)\_)__) \___/(____/                             */
+/*                                                                            */
+/******************************************************************************/
 
-//   ___  __   _  _  ____  __  __    ____    ____  __  _  _  ____
-//  / __)/  \ ( \/ )(  _ \(  )(  )  (  __)  (_  _)(  )( \/ )(  __)
-// ( (__(  O )/ \/ \ ) __/ )( / (_/\ ) _)     )(   )( / \/ \ ) _)
-//  \___)\__/ \_)(_/(__)  (__)\____/(____)   (__) (__)\_)(_/(____)
-//  ____  ____  ____  ____  __  __ _   ___  ____
-// / ___)(  __)(_  _)(_  _)(  )(  ( \ / __)/ ___)
-// \___ \ ) _)   )(    )(   )( /    /( (_ \\___ \
-// (____/(____) (__)  (__) (__)\_)__) \___/(____/
-//
-// Compile time settings
-//
-
-// Which TwoSum algorithm to use
-// 0: 6 addition/subtractions
-// 1: 2 bit extractions, 1 integer conditional, 3 addition/subtractions
+/* Which TwoSum algorithm to use                                              */
+/* 0: 6 addition/subtractions                                                 */
+/* 1: 2 bit extractions, 1 conditional, 3 addition/subtractions               */
 #ifndef TWO_SUM_ALGO
 #define TWO_SUM_ALGO 1
 #endif
 
-// Which TwoProd algorithm to use
-// 0: 2 constant multiplies, 4 multiplies, 10 addition/subtractions
-// 1: 1 multiply, 1 negation, 1 fma
+/* Which conditional to use if TWO_SUM_ALGO == 1                              */
+/* 0: 1 double compare                                                        */
+/* 1: 2 integer extract, 1 integer compare                                    */
+#ifndef TWO_SUM_ONE_COND
+#define TWO_SUM_ONE_COND 1
+#endif
+
+/* Which TwoProd algorithm to use                                             */
+/* 0: 2 constant multiplies, 4 multiplies, 10 addition/subtractions           */
+/* 1: 1 multiply, 1 negation, 1 fma                                           */
 #ifndef TWO_PROD_ALGO
-#ifdef FP_FAST_FMA
+#ifdef FP_FAST_FMA /* If FMA is fast then use it by default */
 #define TWO_PROD_ALGO 1
 #else
 #define TWO_PROD_ALGO 0
 #endif
 #endif
 
-//  ____  _  _  ____  __    __  ___
-// (  _ \/ )( \(  _ \(  )  (  )/ __)
-//  ) __/) \/ ( ) _ (/ (_/\ )(( (__
-// (__)  \____/(____/\____/(__)\___)
-//   __  __ _  ____  ____  ____  ____  __    ___  ____
-//  (  )(  ( \(_  _)(  __)(  _ \(  __)/ _\  / __)(  __)
-//   )( /    /  )(   ) _)  )   / ) _)/    \( (__  ) _)
-//  (__)\_)__) (__) (____)(__\_)(__) \_/\_/ \___)(____)
-//
-// Public interface
-//
+/******************************************************************************/
+/*  ____  _  _  ____  __    __  ___                                           */
+/* (  _ \/ )( \(  _ \(  )  (  )/ __)                                          */
+/*  ) __/) \/ ( ) _ (/ (_/\ )(( (__                                           */
+/* (__)  \____/(____/\____/(__)\___)                                          */
+/*   __  __ _  ____  ____  ____  ____  __    ___  ____                        */
+/*  (  )(  ( \(_  _)(  __)(  _ \(  __)/ _\  / __)(  __)                       */
+/*   )( /    /  )(   ) _)  )   / ) _)/    \( (__  ) _)                        */
+/*  (__)\_)__) (__) (____)(__\_)(__) \_/\_/ \___)(____)                       */
+/*                                                                            */
+/******************************************************************************/
 
+/**
+ * double-double = double + double
+ */
 static void
 Add12(double *sum, double *error,
       double a,
       double b);
 
+/**
+ * double-double = double - double
+ */
 static void
 Sub12(double *diff, double *error,
       double a,
       double b);
 
+/**
+ * double-double = double * double
+ */
 static void
 Mul12(double *prod, double *error,
       double a,
       double b);
 
+/**
+ * double-double = sqrt(double)
+ */
 static void
 Sqrt12(double *ans, double *error,
        double x);
 
+/**
+ * double-double = double-double + double-double
+ */
 static void
 Add22(double *sum_hi, double *sum_lo,
       double a_hi, double a_lo,
       double b_hi, double b_lo);
 
+/**
+ * double-double = double-double - double-double
+ */
 static void
 Sub22(double *diff_hi, double *diff_lo,
       double a_hi, double a_lo,
       double b_hi, double b_lo);
 
+/**
+ * double-double = double-double * double-double
+ */
 static void
 Mul22(double *prod_hi, double *prod_lo,
       double a_hi, double a_lo,
       double b_hi, double b_lo);
 
+/**
+ * double-double = double * double-double
+ */
 static void
 Mul122(double *prod_hi, double *prod_lo,
        double a,
        double b_hi, double b_lo);
 
+/**
+ * double-double = fma(double, double-double, double-double)
+ */
 static void
 MulAdd212(double *fma_hi, double *fma_lo,
           double a,
           double b_hi, double b_lo,
           double c_hi, double c_lo);
 
+/**
+ * double-double = fma(double-double, double-double, double-double)
+ */
 static void
 MulAdd22(double *fma_hi, double *fma_lo,
          double a_hi, double a_lo,
          double b_hi, double b_lo,
          double c_hi, double c_lo);
 
-//  _  _  ____  __    ____  ____  ____  ____
-// / )( \(  __)(  )  (  _ \(  __)(  _ \/ ___)
-// ) __ ( ) _) / (_/\ ) __/ ) _)  )   /\___ \
-// \_)(_/(____)\____/(__)  (____)(__\_)(____/
-//
-// Helpers
-//
+/******************************************************************************/
+/*  _  _  ____  __    ____  ____  ____  ____                                  */
+/* / )( \(  __)(  )  (  _ \(  __)(  _ \/ ___)                                 */
+/* ) __ ( ) _) / (_/\ ) __/ ) _)  )   /\___ \                                 */
+/* \_)(_/(____)\____/(__)  (____)(__\_)(____/                                 */
+/*                                                                            */
+/* Helpers                                                                    */
+/*                                                                            */
+/******************************************************************************/
 
 static inline void
 TwoSum(double *sum, double *error,
@@ -136,8 +198,8 @@ FastTwoSum(double *sum, double *error,
            double a,
            double b)
 {
-    // "... under the assumption that the exponent of a is at least as large as
-    // the exponent of b" - wikipedia
+    /* "... under the assumption that the exponent of a is at least as large  */
+    /*  as the exponent of b" - wikipedia                                     */
     precondition(get_exponent_double(a) >= get_exponent_double(b));
     double z;
     *sum = a + b;
@@ -146,12 +208,27 @@ FastTwoSum(double *sum, double *error,
 }
 
 static inline void
-SafeFastTwoSum(double *sum, double *error,
-               double a,
-               double b)
+DoubleSafeFastTwoSum(double *sum, double *error,
+                     double a,
+                     double b)
 {
-    // "Note that if it is more efficient on a given architecture, the test can
-    // be replaced with a test on the exponents of a and b" - CRLibm
+    if (a >= b)
+    {
+        FastTwoSum(sum, error, a, b);
+    }
+    else
+    {
+        FastTwoSum(sum, error, b, a);
+    }
+}
+
+static inline void
+IntegerSafeFastTwoSum(double *sum, double *error,
+                      double a,
+                      double b)
+{
+    /* "Note that if it is more efficient on a given architecture, the test   */
+    /*  can be replaced with a test on the exponents of a and b" - CRLibm     */
     if (get_exponent_double(a) >= get_exponent_double(b))
     {
         FastTwoSum(sum, error, a, b);
@@ -172,10 +249,18 @@ UsedTwoSum(double *sum, double *error,
 #elif TWO_SUM_ALGO == 0
     TwoSum(sum, error, a, b);
 #elif TWO_SUM_ALGO == 1
-    SafeFastTwoSum(sum, error, a, b);
+#ifndef TWO_SUM_ONE_COND
+#error "Since TWO_SUM_ALGO == 1, TWO_SUM_ONE_COND must be set"
+#elif TWO_SUM_ONE_COND == 0
+    DoubleSafeFastTwoSum(sum, error, a, b);
+#elif TWO_SUM_ONE_COND == 1
+    IntegerSafeFastTwoSum(sum, error, a, b);
+#else
+#error "Invalid TWO_SUM_ONE_COND selection"
+#endif /* ifndef TWO_SUM_ONE_COND */
 #else
 #error "Invalid TWO_SUM_ALGO selection"
-#endif
+#endif /* ifndef TWO_SUM_ALGO */
 }
 
 static inline void
@@ -183,7 +268,7 @@ Split(double *hi, double *lo,
       double a)
 {
     double t, u;
-    t = 134217729.0 * a; // 2**27 + 1
+    t = 134217729.0 * a; /* 2**27 + 1 */
     u = t - a;
     *hi = t - u;
     *lo = a - *hi;
@@ -248,13 +333,15 @@ FastAdd22(double *sum_hi, double *sum_lo,
     *sum_lo = r - (*sum_hi) + s;
 }
 
-//  ____  _  _  __ _   ___  ____  __  __   __ _  ____
-// (  __)/ )( \(  ( \ / __)(_  _)(  )/  \ (  ( \/ ___)
-//  ) _) ) \/ (/    /( (__   )(   )((  O )/    /\___ \
-// (__)  \____/\_)__) \___) (__) (__)\__/ \_)__)(____/
-//
-// Functions
-//
+/******************************************************************************/
+/*  ____  _  _  __ _   ___  ____  __  __   __ _  ____                         */
+/* (  __)/ )( \(  ( \ / __)(_  _)(  )/  \ (  ( \/ ___)                        */
+/*  ) _) ) \/ (/    /( (__   )(   )((  O )/    /\___ \                        */
+/* (__)  \____/\_)__) \___) (__) (__)\__/ \_)__)(____/                        */
+/*                                                                            */
+/* Functions                                                                  */
+/*                                                                            */
+/******************************************************************************/
 
 static inline void
 Add12(double *sum, double *error,
@@ -286,7 +373,7 @@ Sqrt12(double *ans, double *error,
 {
     double s, s2_hi, s2_lo, e;
     s = sqrt(x);
-    e = (x - s*s) / (x + s);
+    e = (x - s * s) / (x + s);
     UsedTwoSum(ans, error, s, e);
 }
 
@@ -371,4 +458,4 @@ MulAdd22(double *fma_hi, double *fma_lo,
     Add12(fma_hi, fma_lo, t3, t10);
 }
 
-#endif // #ifndef DOUBLE_DOUBLE_H
+#endif /* #ifndef DOUBLE_DOUBLE_H */
