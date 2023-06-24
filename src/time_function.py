edeae97f6e21def321831f28a0f92fe9b0329572
yashladd <yashlad.08@gmail.com>
2023-06-23 19:16:43 -0600

    set up

--- src/time_function.py
+++ src/time_function.py
@@ -1,12 +1,48 @@
 
+import subprocess
+from compile import compile_file, link_files
 from interval import Interval
-from numeric_types import NumericType
+from numeric_types import FP32, FP64, NumericType
 
 
 def time_function(numeric_type: NumericType,
-                  points_per_pad: int,
-                  repeats_per_time: int,
                   c_function_name: str,
                   c_code: str,
-                  range: Interval) -> float:
-    return None
+                  domain: Interval,
+                  samples: int = 1 << 17,
+                  iters: int = 100) -> float:
+    
+    if not c_function_name:
+        return None
+    
+    low = numeric_type.num_to_str(domain.inf)
+    high = numeric_type.num_to_str(domain.sup)
+
+
+    if numeric_type == FP64:
+        gen = f"time_function_fp64"
+    elif numeric_type == FP32:
+        gen = f"time_function_fp32"
+
+    lines = [
+        '#include "timing_measurement.h"',
+        '',
+        c_code,
+        '',
+        'int main(int argc, char** argv) {',
+        f'  {gen}({c_function_name}, {low}, {high}, {samples}, {iters});',
+        '}'
+    ]
+    c_source = "\n".join(lines)
+
+    fname = f"{c_function_name}.c"
+    with open(fname, "w") as f:
+        f.write(c_source)
+
+    run_name = f"{c_function_name}_runner"
+    obj = compile_file(fname)
+    runner = link_files([obj], run_name)
+    
+    p = subprocess.run(f"./{runner}", capture_output=True)
+
+    return float.fromhex(p.stdout.decode("utf-8"))
