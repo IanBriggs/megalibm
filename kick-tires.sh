ea440a00e8a72aea17d69f97f24b79592331c31f
Amy Zhu <ajyzhu@gmail.com>
2023-07-12 15:41:27 -0700

    path typo

--- kick-tires.sh
+++ kick-tires.sh
@@ -9,5 +9,5 @@ cp oopsla23/baseline/style.css results/style.css
 cp -r oopsla23/baseline/baseline results/baseline
 cp oopsla23/baseline/index.html results/baseline.html
 
-cp oopsla23/renumo_precomputed/index.html results/renumo_run.html
+cp oopsla23/renumo_precomputed/renumo_run.html results/renumo_run.html
 cp -r oopsla23/renumo_precomputed/generated/ results/generated/
