9cbc6bbc80d5a4dca70addc4d88f49b479e5208f
Amy Zhu <ajyzhu@gmail.com>
2023-07-12 15:40:07 -0700

    add the other precomputed renumo run

--- kick-tires.sh
+++ kick-tires.sh
@@ -8,3 +8,6 @@ python graph_against_baseline.py --directory=oopsla23/tool/
 cp oopsla23/baseline/style.css results/style.css
 cp -r oopsla23/baseline/baseline results/baseline
 cp oopsla23/baseline/index.html results/baseline.html
+
+cp oopsla23/renumo_precomputed/index.html results/renumo_run.html
+cp -r oopsla23/renumo_precomputed/generated/ results/generated/
